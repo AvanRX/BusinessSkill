ad7d08db4e5448c1084426ca74781f5380a7ba59
AvanRX <touyama07@gmail.com>
2019-11-18 15:45:30 +0900

    バリデーションの追加

--- html_and_php/quiz/result.php
+++ html_and_php/quiz/result.php
@@ -5,6 +5,12 @@ require_once("common.php");
 $qid=$_GET['question'];
 $answer=$_GET['answer'];
 
+//バリデーション(要は正しい値かをチェックしてる)
+if($qid==-1||!is_numeric($qid)((0<=$qid)&&($qid<count($question)))){
+    echo 'error : $qid invalid';
+    exit(1);
+}
+
 if($question[$qid][1]==$answer){
     echo "正解！";
 }
